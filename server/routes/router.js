c97db0c1daa500000994839118c06fbb7c1e4ac9
Christofer <christoferguima@gmail.comnam>
2022-02-03 18:47:31 -0300

    better documentation

--- server/routes/router.js
+++ server/routes/router.js
@@ -3,9 +3,24 @@ const route = express.Router();
 
 const services = require("../services/render");
 
+/**
+ * @description Root Rout
+ * @method GET /
+ */
+
 route.get("/", services.homeRoutes);
 
+/**
+ * @description add users
+ * @method GET /add-user
+ */
+
 route.get("/add-user", services.add_user);
+
+/**
+ * @description for update user
+ * @method GET /update-user
+ */
 
 route.get("/update-user", services.update_user);
 
