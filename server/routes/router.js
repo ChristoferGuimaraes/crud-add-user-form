abc030841d848c973102d30150f4f52285562d50
Christofer <christoferguima@gmail.comnam>
2022-02-04 16:28:09 -0300

    API created

--- server/routes/router.js
+++ server/routes/router.js
@@ -2,6 +2,7 @@ const express = require("express");
 const route = express.Router();
 
 const services = require("../services/render");
+const controller = require("../controller/controller");
 
 /**
  * @description Root Rout
@@ -23,5 +24,11 @@ route.get("/add-user", services.add_user);
  */
 
 route.get("/update-user", services.update_user);
+
+//API
+route.post("/api/users", controller.create);
+route.get("/api/users", controller.find);
+route.put("/api/users/:id", controller.update);
+route.delete("/api/users/:id", controller.delete);
 
 module.exports = route;
